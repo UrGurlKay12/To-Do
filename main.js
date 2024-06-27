e1a373b4196f8927f56f14fcbc7e7d0768061980
UrGurlKay12 <kailyn.ray1212@gmail.com>
2024-06-27 11:52:57 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+
+ let todoListItem = document.getElementsByTagName("li");
+ let userName = document.getElementById("userName");
+ let todoTitle = document.getElementById("todoTitle");
+ let todoList = document.getElementById("todoList");
+
+ userName.innerHTML = "Kailyn"; 
+ todoTitle.setAttribute('class', 'newColor'); 
+
+ let newItem = document.createElement("li");
+ newItem.innerHTML = "Write at least 1 page for your paper"; 
+ todoList.appendChild(newItem);
+
+ for (let i = 0; i < todoListItem.length; i++) {
+      todoListItem[i].setAttribute('class', 'listItem');
+
+      if (i == 0) { 
+        todoListItem[i].style.color = "pink";
+    }
+ } 
+
+ todoList.addEventListener('click', function (event) {
+     if(event.target.tagName === "LI") {
+        event.target.remove();
+     }
+ 
+})
